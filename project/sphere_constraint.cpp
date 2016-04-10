3f755e7966a403095ced05d1363ac59d0a0b4f55
Jorin Weatherston <jorinw@ecs354-04.csc.uvic.ca>
2016-04-10 16:14:22 -0700

    Circle_sphere_base_constraint and children added and inheriting correctly.

---
+++ project/sphere_constraint.cpp
@@ -0,0 +1,11 @@
+#include <csc486a/sphere_constraint.hpp>
+
+
+namespace csc486a {
+
+    sphere_constraint::sphere_constraint (const OpenGP::SurfaceMesh & mesh, std::vector<OpenGP::SurfaceMesh::Vertex> vs, float w, type) : circle_sphere_constraint_base(mesh, std::move(vs), w, type::sphere) { }
+
+
+
+}
+
