b6e402097cfe6aceb95b2faa725623abe40a40bb
Jordan Heemskerk <jordanh@shaw.ca>
2016-04-13 09:41:25 -0700

    rigid-demo: just like the video now
    - closeness holds mesh to its current converged form, not the original
    - weight the isometric onerings at way less than closeness of handles

--- project/main.cpp
+++ project/main.cpp
@@ -66,12 +66,6 @@ namespace {
             // copy the original to displaced
             OpenGP::SurfaceMesh original = OpenGP::SurfaceMesh(mesh_);
 
-
-            
-            move_handle(Eigen::Vector3f(0,-0.4,-0.4));
-            
-            // move some vertices around
-
             
             // define handle
             OpenGP::SurfaceMesh::Vertex_property<bool> is_handle = mesh_.add_vertex_property<bool>("v:is_handle", false);
@@ -152,10 +146,10 @@ namespace {
             ccs_.emplace_back(mesh_,OpenGP::SurfaceMesh::Vertex(2),1.0f);
             
             
-            //loop over onerings and add constraint for each one
+            //loop over onerings and add constraint for each one. weight this less than holding the handles
             unsigned int curr = 0;
             std::vector<OpenGP::SurfaceMesh::Vertex> onering;
-            onering.reserve(10);
+            onering.reserve(10); // should be sufficient for quad and triangle
             for (auto && v : mesh.vertices()) {
 
                 //if (is_handle[v]) continue;
@@ -167,23 +161,26 @@ namespace {
                     onering.push_back(v_or);
                 }
 
-                rcs_.emplace_back(mesh_, original, onering, 1.0f);
+                rcs_.emplace_back(mesh_, original, onering, 0.05f);
                 add(rcs_.back());
                 
-                //progress report
+                /*//progress report (for large meshes)
                 if (curr % (mesh.n_vertices()/20) == 0) {
                     float prog = (curr * 100.0)/mesh.n_vertices();
                     std::cout << "Onering Progress: " << prog << std::endl;
                 }
-                curr++;
+                curr++;*/
             }
             
-            //for (auto && cc : ccs_) add(cc);
+            // add closeness constriants
+            for (auto && cc : ccs_) add(cc);
+            
+            // now triangulate and add to scene
             add_mesh_to_scene();
             
         }
         
-        
+#define IT 8
         void key_callback (int key, int scancode, int action, int mods) {
             
             window_base::key_callback(key, scancode, action, mods);
@@ -193,6 +190,7 @@ namespace {
             if ((key==GLFW_KEY_UP) && (action==GLFW_RELEASE)) {
                 offset = Eigen::Vector3f(0,-0.1,0);
                 move_handle(offset);
+                for(int i = 0; i < IT; i++)s_();
                 mesh_.update_face_normals();
                 renderer_.init_data();
                 
@@ -201,6 +199,7 @@ namespace {
             if ((key==GLFW_KEY_LEFT) && (action==GLFW_RELEASE)) {
                 offset = Eigen::Vector3f(0,0,-0.1);
                 move_handle(offset);
+                for(int i = 0; i < IT; i++)s_();
                 mesh_.update_face_normals();
                 renderer_.init_data();
             }
@@ -208,6 +207,7 @@ namespace {
             if ((key==GLFW_KEY_DOWN) && (action==GLFW_RELEASE)) {
                 offset = Eigen::Vector3f(0,0.1,0);
                 move_handle(offset);
+                for(int i = 0; i < IT; i++)s_();
                 mesh_.update_face_normals();
                 renderer_.init_data();
             }
@@ -215,6 +215,7 @@ namespace {
             if ((key==GLFW_KEY_RIGHT) && (action==GLFW_RELEASE)) {
                 offset = Eigen::Vector3f(0,0,0.1);
                 move_handle(offset);
+                for(int i = 0; i < IT; i++)s_();
                 mesh_.update_face_normals();
                 renderer_.init_data();
             }
